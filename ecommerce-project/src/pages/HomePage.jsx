ab59dc1d84ac763f86132319f74d284183318857
Abhilash Reddy <reddyabhilash990@gmail.com>
2026-06-04 16:37:53 +0530

    Generate the html for the checkout item or cart items

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState} from 'react';
 import { Header } from '../components/header';
+import { formatMoney } from '../utils/money';
 
 import './Homepage.css';
 
@@ -54,7 +55,7 @@ export function HomePage({ cart }) {
                                 </div>
 
                                 <div className="product-price">
-                                    {(product.priceCents / 100).toFixed(2)}
+                                    {formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
