7919799f4dcf980f5a729f03015e86c4c1a21d7a
Abhilash Reddy <reddyabhilash990@gmail.com>
2026-06-08 13:55:01 +0530

    covert home , checkout, orderpage to async await

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,11 +12,15 @@ import './App.css'
 function App() {
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product') 
-      .then((response) => {
-        setCart(response.data);
+    const fetchAppData =  async() => {
+      const response = await axios.get('/api/cart-items?expand=product') 
+      setCart(response.data);
 
-      })
+      
+    };
+
+    fetchAppData();
+    
   }, []);
 
 
